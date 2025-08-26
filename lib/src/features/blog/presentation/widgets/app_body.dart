4b1ede50c99e9507ff575604b38956716fa4754e
coding-faizan <faizanahmad8292@gmail.com>
2025-08-26 19:37:58 +0500

    async value duplication removal

--- lib/src/features/blog/presentation/widgets/app_body.dart
+++ lib/src/features/blog/presentation/widgets/app_body.dart
@@ -1,27 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_blog/src/features/blog/data/repository/article_repository.dart';
 import 'package:flutter_blog/src/features/blog/presentation/screens/favourites_screen.dart';
 import 'package:flutter_blog/src/features/blog/presentation/screens/watched_articles_screen.dart';
-import 'package:flutter_blog/src/features/blog/presentation/widgets/custom_error_widget.dart';
 import 'package:flutter_blog/src/features/blog/presentation/widgets/home_tab.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class AppBody extends ConsumerWidget {
+class AppBody extends StatelessWidget {
   const AppBody({required this.currentIndex, super.key});
 
   final int currentIndex;
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final articlesList = ref.watch(articlesListProvider);
-    return articlesList.when(
-      data: (data) => <Widget>[
-        HomeTab(),
-        FavouritesScreen(),
-        WatchedArticlesScreen(),
-      ][currentIndex],
-      error: (error, st) => CustomErrorWidget(error: error),
-      loading: () => Center(child: CircularProgressIndicator()),
-    );
+  Widget build(BuildContext context) {
+    return <Widget>[
+      HomeTab(),
+      FavouritesScreen(),
+      WatchedArticlesScreen(),
+    ][currentIndex];
   }
 }
